697abd2f16b3ab8b8570cc81b2507ce90ada7846
Vanessa Sochat <vsochat@stanford.edu>
2019-01-04 15:06:08 -0500

    testing better solution for circle urls

---
+++ .circleci/urls.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from glob import glob
+import os
+
+# A quick script to fix urls for circle
+baseurl="https://6-163772993-gh.circle-artifacts.com/0/_site"
+
+here = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+print('Working directory is %s' % here)
+
+files = glob('%s/_layouts/*.html' % here) +
+        glob('%s/_includes/*.html' % here) +
+        glob('%s/pages/*.md' % here)
+
+# https://circleci.com/docs/2.0/env-vars/#built-in-environment-variables
+running_ci = os.environ.get('CI', False)
+
+if running_ci in ['true', True]:
+    print('Detected running on CircleCi, fixing static files')
+    # Only run if we are in CI environment
+
+    for file_name in files:
+
+        print('Fixing URL in %s' % file_name)
+        # 1. Read in the file
+        with open(file_name, 'r') as filey:
+            content = filey.read()
+
+        # 2. Replace with the actual baseurl
+        content = content.replace('{{ site.uri }}', baseurl)
+
+        # 3. Save the file
+        with open(file_name, 'w') as filey:
+            filey.writelines(content)
--- .circleci/urls.sh
+++
@@ -1,3 +0,0 @@
-BASEURL=https://circle-artifacts.com/gh/penrose-lib/penrose-lib.github.com/$CIRCLE_BUILD_NUM/artifacts/0/home/ubuntu/penrose-lib.github.io/_site
-sed -i "14 s,.*,destination: ./_site,g" "config.yml"
-sed -i "11 s,.*,baseurl: $BASEURL,g" "config.yml"
